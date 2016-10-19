200fa259aafda0d5f11a585d67da8db46c7253e0
Gordan Savicic <gordo@yugo.at>
2016-10-19 12:13:22 +0200

    new york times

--- week03/004_loadJSONAPI/sketch.js
+++ week03/004_loadJSONAPI/sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
+  var searchWord = "clinton";
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?';
+  url += "api-key=7325774b17fa420eace0388afb00b628";
+  url += "&q=" + searchWord;
+  loadJSON(url, drawData);
 }
 
 function draw() {
-  
+
+}
+
+function drawData(data) {
+  print(data.response.docs);
+  var articles = data.response.docs;
+  for (var i=0; i< articles.length; i++) {
+    var snippet = articles[i].snippet;
+    text(snippet, 10, 40 *i);
+  }
+
 }
