8ac78e9cb3343f893f6d056e8227480511890fad
Gordan Savicic <gordo@yugo.at>
2016-10-19 11:50:30 +0200

    circle diagram

--- week03/003_loadJSON/sketch.js
+++ week03/003_loadJSON/sketch.js
@@ -1,7 +1,33 @@
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
+  loadJSON("data.json",drawData);
+  print("... setting up ...");
+  textAlign(CENTER,CENTER);
+  textSize(18);
+  noStroke();
 }
 
 function draw() {
-  
+
+}
+
+function drawData(data) {
+  print(".. data loaded ...");
+  var i = 1;
+  for (var index in data) {
+    //print(data[index].location);
+    var age = data[index].age * 1;
+    var ageCorrect = Math.sqrt(age / Math.PI) * 10;
+    var name = data[index].name;
+    var colour = data[index].colour;
+
+    fill(colour);
+    fill (random(255),random(255),random(255));
+    ellipse(100*i,100,ageCorrect,ageCorrect);
+    ellipse(100*i,300,age,age);
+    fill(0);
+    text(name,100*i,150);
+    //debugger;
+    i += 1;
+  }
 }
